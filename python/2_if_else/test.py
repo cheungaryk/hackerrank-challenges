d6f91d0a5e78be4bc223ffb51fc3e32471a770ba
Gary Cheung <cheungaryk@protonmail.com>
2020-03-21 14:41:05 -0700

    fixed errors and pipeline

--- python/2_if_else/test.py
+++ python/2_if_else/test.py
@@ -4,6 +4,7 @@ import unittest
 
 from main import check_weirdness
 
+
 class TestCheckWeirdness(unittest.TestCase):
     def test_odd_1(self):
         result = check_weirdness(1)
@@ -12,15 +13,15 @@ class TestCheckWeirdness(unittest.TestCase):
     def test_odd_2_to_5(self):
         result = check_weirdness(3)
         self.assertEqual(result, "Weird")
-    
+
     def test_odd_6_to_20(self):
         result = check_weirdness(19)
         self.assertEqual(result, "Weird")
-    
+
     def test_odd_gt_than_20(self):
         result = check_weirdness(23)
         self.assertEqual(result, "Weird")
-    
+
     def test_even_2_to_5(self):
         result = check_weirdness(4)
         self.assertEqual(result, "Not Weird")
@@ -28,15 +29,15 @@ class TestCheckWeirdness(unittest.TestCase):
     def test_even_6_to_20(self):
         result = check_weirdness(10)
         self.assertEqual(result, "Weird")
-    
+
     def test_even_gt_than_20(self):
         result = check_weirdness(22)
         self.assertEqual(result, "Not Weird")
-    
+
     def test_less_than_1(self):
         result = check_weirdness(-1)
         self.assertEqual(result, "Not Applicable")
-    
+
     def test_gt_than_100(self):
         result = check_weirdness(101)
         self.assertEqual(result, "Not Applicable")
